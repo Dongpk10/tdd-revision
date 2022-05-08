9c37447b6acd51b4d6df80b75a9320de2e3582be
Dongpk10 <vandong.tran@ynov.com>
2022-05-08 18:35:59 +0200

    003

--- app/js/string_cal.js
+++ app/js/string_cal.js
@@ -3,10 +3,43 @@ export class StringCalculator {
     add(str) {
         if (str.length === 0) return 0;
         if (str.length === 1) return parseInt(str);
+        let delimiters=this.getdelimiter(str);
 
-        let numbers = str.split(',').map(ele => parseInt(ele));
+
+        str=this.get_number_part(str);
+        delimiters.forEach(element => {
+            str=str.replaceAll(element,",");
+        })
+
+
+        let numbers = str.replaceAll("\n",",").split(',').map(ele => parseInt(ele)).filter(ele=>ele<=1000);
+        let neg_number=numbers.filter(ele => ele<0);
+        if (neg_number.length>0) throw Error(`Negatives not allowed. [${neg_number.join(",")}]`);
+
         return numbers.reduce((sum, currr) => sum+currr);
 
     }
+    hasdelimiter(str){
+        return str.includes("//");
+    }
+    getdelimiter(str){
+        if (this.hasdelimiter(str)) {
+            if (str.includes('[')) {
+                return str.split(/\[(.*?)\]/).filter((elem, key) => (key % 2 !== 0));
+            } else {
+                return [str.charAt(2)];
+            }
+        }
+        return [','];
+    }
+    get_number_part(str){
+        if(this.hasdelimiter(str)){
+            return str.split(/\n(.*)/s)[1];
+        }
+        return str;
+    }
 
 }
+let str = '//[***]\n1***2***3';
+console.log(str.split(/\[(.*?)\]/));
+console.log(str.split(/\[(.*?)\]/).filter((elem, key) => (key % 2 !== 0)))
--- app/tests/string_cal.test.js
+++ app/tests/string_cal.test.js
@@ -20,5 +20,35 @@ describe('string calculator tdd', () => {
         expect(stringCalculator.add("1,2,3")).toBe(6);
 
     });
+    test("for //;\n1;2 return 3",()=>{
+        expect(stringCalculator.add('//;\n1;2')).toBe(3);
+
+    });
+    test("for '1\n2,3' return 6",()=>{
+        expect(stringCalculator.add("1\n2,3")).toBe(6);
+
+    });
+    test("for '1\n-2,3' throw error",()=>{
+        expect(() => (stringCalculator.add('1\n-2,3'))).toThrow();
+        expect(() => (stringCalculator.add('1\n-2,3'))).toThrow(Error);
+        expect(() => (stringCalculator.add('1\n-2,3'))).toThrow('Negatives not allowed. [-2]');
+
+    });
+    test("for '1\n-2,3,-4' thow error",()=>{
+        expect(() => (stringCalculator.add('1\n-2,3,-4'))).toThrow();
+        expect(() => (stringCalculator.add('1\n-2,3,-4'))).toThrow(Error);
+        expect(() => (stringCalculator.add('1\n-2,3,-4'))).toThrow('Negatives not allowed. [-2,-4]');
+
+    });
+    test("ignore number >1000",()=>{
+        expect(stringCalculator.add('1\n,2,1004')).toBe(3);
+    });
+    test('for "//[***]\n1***2***3" returns 6',()=>{
+        expect(stringCalculator.add("//[***]\n1***2***3")).toBe(6);
+    });
+    test('//[**][%%]\n1**2%%3" returns 6',()=>{
+        expect(stringCalculator.add("//[**][%%]\n1**2%%3")).toBe(6);
+    });
+
 
 });
